9eb1694bf746582d8bfe438e87ac215f5104aab1
pverma-turing <prashant.v@turing.com>
2025-06-11 20:34:57 +0530

    conversation-2_interaction-9_model_A

--- wordwise/commands/__init__.py
+++ wordwise/commands/__init__.py
@@ -6,3 +6,4 @@ from wordwise.commands.lookup import LookupCommand
 from wordwise.commands.save import SaveCommand
 from wordwise.commands.view import ViewCommand
 from wordwise.commands.edit import EditCommand
+from wordwise.commands.export import ExportCommand
